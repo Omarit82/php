d0f600b28bd5c9196dbd56ffe4dbb32944bba92a
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2024-04-17 16:55:30 -0300

    Primera clase php

---
+++ Clase1-ArquitecturaWeb/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+        <p>Pagina dinamica</p>
+
+        <?php
+            /** Variables **/
+            $nombre = "Omar Roselli";
+            echo "<h2>".$nombre."</h2>";
+
+            /** Arreglos  **/
+            $arregloNombres = ["Fernando", "Franco", "Carlos", "Martin"];
+            echo"<ul>";
+            for($i=0; $i<count($arregloNombres); $i++){
+                echo "<li>". $arregloNombres[$i]."</li>";
+            }
+            echo"</ul>";
+
+            /** Constantes **/
+            define("OMAR","Omar Roselli");
+            echo OMAR;
+
+            
+        ?>
+
+
+    </body>
+</html>
